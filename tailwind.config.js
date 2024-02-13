62786deef1fa03b639e9ebb484219261d3a1b160
David Mutua <mutuadavi@gmail.com>
2024-02-13 12:24:09 +0300

    done with Maths games and quizzes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "0 2px 0 rgba(0,0,0,1), inset 0 -1px 4px rgba(0,0,0,.5)"
+      },
+      colors: {
+        "text-title": "#232323"
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
