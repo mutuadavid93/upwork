7164fbf807465e259a00e62c646c98d2b1a9124e
David Mutua <mutuadavi@gmail.com>
2024-02-13 15:46:24 +0300

    add footer final element

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        custom: "0 2px 0 rgba(0,0,0,1), inset 0 -1px 4px rgba(0,0,0,.5)"
+        custom: "0 2px 0 rgba(0,0,0,1), inset 0 -1px 4px rgba(0,0,0,.5)",
       },
       colors: {
         "text-title": "#232323"
