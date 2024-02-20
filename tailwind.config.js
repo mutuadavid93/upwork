aa943d7d3b1544468f0bf965c4da6c129ec51892
David Mutua <mutuadavi@gmail.com>
2024-02-20 14:26:42 +0300

    ft: work on lumniz home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       boxShadow: {
         custom: "0 2px 0 rgba(0,0,0,1), inset 0 -1px 4px rgba(0,0,0,.5)",
         quiz: "0 0 10px rgba(0,0,0,.1)",
-        container: "-0.0625rem 0.25rem 0 rgba(0,0,0,.18)"
+        container: "-0.0625rem 0.25rem 0 rgba(0,0,0,.18)",
       },
       colors: {
         "text-title": "#232323",
