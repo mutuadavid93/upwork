e7adf98d0d66b23375bfd4443a84cd627f60819d
David Mutua <mutuadavi@gmail.com>
2024-02-21 15:59:57 +0300

    merging

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         custom: "0 2px 0 rgba(0,0,0,1), inset 0 -1px 4px rgba(0,0,0,.5)",
         quiz: "0 0 10px rgba(0,0,0,.1)",
         container: "-0.0625rem 0.25rem 0 rgba(0,0,0,.18)",
+        slider: "0 0.125rem 0 var(--black),0 0.375rem 0 rgba(0,0,0,.12),inset 0 -0.0625rem 0.25rem var(--black)"
       },
       colors: {
         "text-title": "#232323",
